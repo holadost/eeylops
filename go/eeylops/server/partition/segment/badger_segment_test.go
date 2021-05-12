f933237246d7bd9df976a35f5aa5383f8a6c1dc6
Nikhil <nikhil-8987@github.com>
2021-05-11 17:16:33 -0700

    Added initial test skeleton for badger segment.

--- go/eeylops/server/partition/segment/badger_segment_test.go
+++ go/eeylops/server/partition/segment/badger_segment_test.go
@@ -1,1 +1,31 @@
 package segment
+
+import "testing"
+
+func TestNewBadgerSegment(t *testing.T) {
+
+}
+
+func TestBadgerSegment_Append(t *testing.T) {
+
+}
+
+func TestBadgerSegment_Initialize(t *testing.T) {
+
+}
+
+func TestBadgerSegment_Close(t *testing.T) {
+
+}
+
+func TestBadgerSegment_Metadata(t *testing.T) {
+
+}
+
+func TestBadgerSegment_SetImmutable(t *testing.T) {
+
+}
+
+func TestBadgerSegment_Scan(t *testing.T) {
+
+}
