3ed3ff1eb2967c2e3f77c6ff17eca52be3e7daa7
Nikhil <nikhil-8987@github.com>
2021-05-17 21:02:36 -0700

    Initial parts of partition is ready. Some minor changes to segment to also return the next offset when testing some stuff

--- go/eeylops/server/storage/partition.go
+++ go/eeylops/server/storage/partition.go
@@ -5,23 +5,102 @@ import (
 )
 
 type Partition struct {
-	segments    []Segment
-	liveSegment Segment
-	segmentLock sync.Mutex
+	segments     []*segmentEntry // List of segments in the partition.
+	segmentsLock sync.RWMutex    // This mutex guards segments and segmentIntervals slice.
+}
+
+type segmentEntry struct {
+	segment Segment
+	segLock sync.RWMutex // This guards read/write access to the segment.
 }
 
 func NewPartition() (*Partition, error) {
 	return nil, nil
 }
 
-func (p *Partition) Initialize() {
-
+func (p *Partition) initialize() {
 }
 
-func (p *Partition) GetSegments() []Segment {
-	return nil
+// GetSegments returns a list of segments that contains all the elements between the given start and end offsets.
+func (p *Partition) GetSegments(startOffset uint64, endOffset uint64) []Segment {
+	p.segmentsLock.RLock()
+	defer p.segmentsLock.RUnlock()
+	var segs []Segment
+
+	// Find start offset segment.
+	startSegIdx := p.findOffset(0, len(p.segments)-1, startOffset)
+	if startSegIdx == -1 {
+		// We did not find any segments that contains our offsets.
+		return segs
+	}
+
+	// Find the end offset segment. Finding the end offset is split into two paths: fast and slow.
+	// This is fast path. For the most part, the endIdx is going to be in the start or the next couple of
+	// segments right after start. So we quickly check that and if it isn't there, we fall back to scanning
+	// all segments.
+	endSegIdx := -1
+	for ii := startSegIdx; ii < startSegIdx+3; ii++ {
+		if ii >= len(p.segments) {
+			break
+		}
+		if p.offsetInSegment(endOffset, p.segments[ii].segment.GetMetadata()) {
+			endSegIdx = ii
+			break
+		}
+	}
+	// Slow path.
+	if endSegIdx == -1 {
+		endSegIdx = p.findOffset(startSegIdx, len(p.segments)-1, endOffset)
+	}
+
+	// Populate segments.
+	segs = append(segs, p.segments[startSegIdx].segment)
+	if endSegIdx == -1 || startSegIdx == endSegIdx {
+		return segs
+	}
+	for ii := startSegIdx + 1; ii <= endSegIdx; ii++ {
+		segs = append(segs, p.segments[ii].segment)
+	}
+	return segs
 }
 
+// GetLiveSegment returns the current live segment.
 func (p *Partition) GetLiveSegment() Segment {
-	return nil
+	p.segmentsLock.RLock()
+	defer p.segmentsLock.RUnlock()
+	return p.segments[len(p.segments)-1].segment
+}
+
+// findOffset finds the segment index that contain the given offset. This function assumes that a
+// read lock has been acquired on segments.
+func (p *Partition) findOffset(startIdx int, endIdx int, offset uint64) int {
+	// Base cases.
+	if startIdx > endIdx {
+		return -1
+	}
+	if startIdx == endIdx {
+		metadata := p.segments[startIdx].segment.GetMetadata()
+		if p.offsetInSegment(offset, metadata) {
+			return startIdx
+		}
+		return -1
+	}
+	midIdx := startIdx + (endIdx-startIdx)/2
+	metadata := p.segments[midIdx].segment.GetMetadata()
+	if p.offsetInSegment(offset, metadata) {
+		return midIdx
+	} else if offset < metadata.StartOffset {
+		// Search to left tree.
+		return p.findOffset(0, midIdx-1, offset)
+	} else {
+		// Search in right tree.
+		return p.findOffset(midIdx+1, endIdx, offset)
+	}
+}
+
+func (p *Partition) offsetInSegment(offset uint64, metadata SegmentMetadata) bool {
+	if offset >= metadata.StartOffset && offset <= metadata.EndOffset {
+		return true
+	}
+	return false
 }
