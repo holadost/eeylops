832ad390b98b5bf8fcab6e80295e1f857a454b76
Nikhil <nikhil-8987@github.com>
2021-05-28 17:42:50 -0700

    Added test function names and signatures. Cleaned up some unrequired files.

--- go/eeylops/server/storage/partition_test.go
+++ go/eeylops/server/storage/partition_test.go
@@ -1,1 +1,37 @@
 package storage
+
+import (
+	"testing"
+)
+
+func createTestPartitionDir(t *testing.T, testName string) string {
+	return ""
+}
+
+func checkPartitionMetadata(t *testing.T) {
+
+}
+
+func TestPartitionInitialize(t *testing.T) {
+
+}
+
+func TestPartitionReInitialize(t *testing.T) {
+
+}
+
+func TestPartitionAppend(t *testing.T) {
+
+}
+
+func TestPartitionScan(t *testing.T) {
+
+}
+
+func TestPartitionNewSegmentCreation(t *testing.T) {
+
+}
+
+func TestStress(t *testing.T) {
+
+}
