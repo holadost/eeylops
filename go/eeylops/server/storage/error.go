0336adfa9b23844b079e811d18c8f57375a95ab6
Nikhil <nikhil-8987@github.com>
2021-06-05 00:25:41 -0700

    Added error codes to error as well. We can now use this to return storage errors.

--- go/eeylops/server/storage/error.go
+++ go/eeylops/server/storage/error.go
@@ -1,19 +1,49 @@
 package storage
 
-type StorageError struct {
-	msg string
+import "fmt"
+
+type StoreErrorCode int64
+
+const (
+	KVStoreKeyNotFoundErr StoreErrorCode = 1
+	KVStoreErr            StoreErrorCode = 2
+	SegmentErr            StoreErrorCode = 3
+	PartitionErr          StoreErrorCode = 4
+	ConsumerStoreErr      StoreErrorCode = 5
+	TopicStoreErr         StoreErrorCode = 6
+)
+
+func (sec StoreErrorCode) ToString() string {
+	switch sec {
+	case KVStoreKeyNotFoundErr:
+		return "KVStoreKeyNotFoundErr"
+	case KVStoreErr:
+		return "KVStoreErr"
+	case SegmentErr:
+		return "SegmentErr"
+	case PartitionErr:
+		return "PartitionErr"
+	case ConsumerStoreErr:
+		return "ConsumerStoreErr"
+	case TopicStoreErr:
+		return "TopicStoreErr"
+	default:
+		return "Invalid error code!"
+	}
 }
 
-func NewStorageError(msg string) *StorageError {
-	se := &StorageError{}
+type StoreError struct {
+	msg string
+	ec  StoreErrorCode
+}
+
+func NewStorageError(msg string, ec StoreErrorCode) *StoreError {
+	se := &StoreError{}
 	se.msg = msg
+	se.ec = ec
 	return se
 }
 
-func (se *StorageError) Error() string {
-	return se.msg
-}
-
-func (se *StorageError) Trace() string {
-	return ""
+func (se *StoreError) Error() string {
+	return fmt.Sprintf("storage error[%s]: %s ", se.ec.ToString(), se.msg)
 }
