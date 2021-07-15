20e7efd19f56c3c36fc759d86bb313f31c9b353c
Nikhil <nikhil-8987@github.com>
2021-07-15 14:46:01 -0700

    Added an error code.

--- go/eeylops/server/storage/error.go
+++ go/eeylops/server/storage/error.go
@@ -17,6 +17,9 @@ var (
 	// ErrGenericSegment when we have an unexpected segment error.
 	ErrGenericSegment = errors.New("ErrGenericSegment: generic segment error")
 
+	// ErrSegmentBackend when we have an unexpected segment error.
+	ErrSegmentBackend = errors.New("ErrSegmentBackend: segment backend error")
+
 	// ErrSegmentClosed is returned if we try to access the segment when it is closed.
 	ErrSegmentClosed = errors.New("ErrSegmentClosed: segment is closed")
 
