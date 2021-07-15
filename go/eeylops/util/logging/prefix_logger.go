55200ad31ae1b8a9ad868e361314d8c7e765d1f6
Nikhil <nikhil-8987@github.com>
2021-07-14 22:31:11 -0700

    Added a new logging directory that also provides a prefix logger. Had to modify glog(changed stack level from 3(hardcoded) to 4) to make this happen.

---
+++ go/eeylops/util/logging/prefix_logger.go
@@ -0,0 +1,57 @@
+package logging
+
+import (
+	"fmt"
+)
+
+type PrefixLogger struct {
+	prefix string // The prefix string that is attached to every log statement.
+}
+
+// NewPrefixLogger returns a new instance of the prefix logger.
+func NewPrefixLogger(prefix string) *PrefixLogger {
+	logger := PrefixLogger{prefix: createPrefixStr(prefix)}
+	return &logger
+}
+
+// NewPrefixLoggerWithParent returns a new instance of the prefix logger. It uses the prefix of the parent as well
+// as the given prefix in every log statement. Makes traceability from logs a whole lot easier.
+func NewPrefixLoggerWithParent(prefix string, parentLogger *PrefixLogger) *PrefixLogger {
+	newLogger := PrefixLogger{
+		prefix: parentLogger.GetPrefix() + " " + createPrefixStr(prefix),
+	}
+	return &newLogger
+}
+
+func (logger *PrefixLogger) GetPrefix() string {
+	return logger.prefix
+}
+
+func (logger *PrefixLogger) Infof(format string, args ...interface{}) {
+	newFormat := fmt.Sprintf("%s %s", logger.prefix, format)
+	Infof(newFormat, args...)
+}
+
+func (logger *PrefixLogger) Errorf(format string, args ...interface{}) {
+	newFormat := fmt.Sprintf("%s %s", logger.prefix, format)
+	Errorf(newFormat, args...)
+}
+
+func (logger *PrefixLogger) Warningf(format string, args ...interface{}) {
+	newFormat := fmt.Sprintf("%s %s", logger.prefix, format)
+	Warningf(newFormat, args...)
+}
+
+func (logger *PrefixLogger) Fatalf(format string, args ...interface{}) {
+	newFormat := fmt.Sprintf("%s %s", logger.prefix, format)
+	Fatalf(newFormat, args...)
+}
+
+func (logger *PrefixLogger) VInfof(v uint, format string, args ...interface{}) {
+	newFormat := fmt.Sprintf("%s %s", logger.prefix, format)
+	V(Level(v)).Infof(newFormat, args...)
+}
+
+func createPrefixStr(prefix string) string {
+	return "[" + prefix + "]"
+}
