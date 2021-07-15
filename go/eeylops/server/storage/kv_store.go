337cf3bc835f0b059f59313297f06b35f83ecfae
Nikhil <nikhil-8987@github.com>
2021-07-15 14:45:41 -0700

    Modified interfaces to KV store.

--- go/eeylops/server/storage/kv_store.go
+++ go/eeylops/server/storage/kv_store.go
@@ -2,6 +2,7 @@ package storage
 
 type KVStore interface {
 	GetDataDir() string
+	Close() error
 	// Get and GetS gets the value for the given key from the store.
 	Get([]byte) ([]byte, error)
 	GetS(string) (string, error)
@@ -19,15 +20,15 @@ type KVStore interface {
 	MultiGetS([]string) ([]string, []error)
 
 	// BatchPut and BatchPutS does batch Put and PutS .
-	BatchPut([][]byte, [][]byte) []error
-	BatchPutS([]string, []string) []error
+	BatchPut([][]byte, [][]byte) error
+	BatchPutS([]string, []string) error
 
 	// BatchDelete and BatchDeleteS does Delete and DeleteS .
-	BatchDelete([][]byte) []error
-	BatchDeleteS([]string) []error
+	BatchDelete([][]byte) error
+	BatchDeleteS([]string) error
 
 	// Scan and ScanS scans values from the store starting from the given start key and num keys. Every scan call
 	// returns a slice of keys, values, the next key and scan error(if any).
-	Scan([]byte, int) ([][]byte, [][]byte, []byte, error)
-	ScanS(string, int) ([]string, []string, string, error)
+	Scan([]byte, int, int, bool) ([][]byte, [][]byte, []byte, error)
+	ScanS(string, int, int, bool) ([]string, []string, string, error)
 }
