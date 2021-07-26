c46a323f45ebe2c30cbbaf5f1ea021b00b55e538
Nikhil <nikhil-8987@github.com>
2021-07-26 13:56:25 -0700

    Added a partition scanner class. Mostly to be used along with unit tests and if required in the future with eeylops sessions.

---
+++ go/eeylops/server/storage/partition_scanner.go
@@ -0,0 +1,90 @@
+package storage
+
+import (
+	"context"
+	"eeylops/server/base"
+	sbase "eeylops/server/storage/base"
+	"github.com/golang/glog"
+)
+
+type PartitionScanner struct {
+	p                 *Partition
+	startOffset       base.Offset
+	lastNextOffset    base.Offset
+	lastValuesScanned []*sbase.ScanValue
+	lastErrorScanned  error
+}
+
+func NewPartitionScanner(p *Partition, startOffset base.Offset) *PartitionScanner {
+	var ps PartitionScanner
+	ps.p = p
+	ps.startOffset = startOffset
+	return &ps
+}
+
+func (ps *PartitionScanner) Rewind() {
+	ps.lastNextOffset = ps.startOffset
+	ps.getVal()
+}
+
+func (ps *PartitionScanner) Valid() bool {
+	if ps.lastErrorScanned != nil {
+		return false
+	}
+	if ps.lastValuesScanned != nil {
+		// We still have a value that has not been read. The iterator is still valid.
+		return true
+	}
+	if ps.lastNextOffset == -1 {
+		return false
+	}
+	return (ps.lastNextOffset != -1) && (ps.lastErrorScanned == nil)
+}
+
+func (ps *PartitionScanner) Next() {
+	if ps.lastNextOffset == -1 || ps.lastErrorScanned != nil {
+		return
+	}
+	ps.getVal()
+}
+
+func (ps *PartitionScanner) Get() (*sbase.ScanValue, error) {
+	if ps.lastErrorScanned != nil {
+		return nil, ps.lastErrorScanned
+	}
+	if len(ps.lastValuesScanned) != 1 {
+		glog.Fatalf("Expected one value. Got %d", len(ps.lastValuesScanned))
+	}
+	val := ps.lastValuesScanned[0]
+	ps.lastValuesScanned = nil
+	return val, nil
+}
+
+func (ps *PartitionScanner) Seek(offset base.Offset) {
+	if ps.lastErrorScanned != nil {
+		return
+	}
+	if offset >= ps.startOffset {
+		ps.lastNextOffset = offset
+	} else {
+		glog.Fatalf("Invalid offset: %d lesser than start offset of scanner: %d", offset, ps.startOffset)
+	}
+	ps.lastValuesScanned = nil
+	ps.getVal()
+}
+
+func (ps *PartitionScanner) getVal() {
+	if ps.lastNextOffset == -1 {
+		glog.Fatalf("Unexpected lastNextOffset: %d", ps.lastNextOffset)
+	}
+	arg := sbase.ScanEntriesArg{
+		StartOffset:    ps.lastNextOffset,
+		NumMessages:    1,
+		StartTimestamp: -1,
+		EndTimestamp:   -1,
+	}
+	ret := ps.p.Scan(context.Background(), &arg)
+	ps.lastValuesScanned = ret.Values
+	ps.lastNextOffset = ret.NextOffset
+	ps.lastErrorScanned = ret.Error
+}
