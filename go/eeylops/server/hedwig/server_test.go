b9bfb98bb1520ada2fdec23ee9bf705b0ff85542
Nikhil <nikhil-8987@github.com>
2021-08-05 14:42:53 -0700

    Modified the command to generate proto.

--- go/eeylops/server/hedwig/server_test.go
+++ go/eeylops/server/hedwig/server_test.go
@@ -1,6 +1,5 @@
 package hedwig
 
 /*
-protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative eeylops/server/hedwig/playgrpc/greet/greetpb/greet.proto
-
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative server/hedwig/playgrpc/greet/greet.proto
 */
