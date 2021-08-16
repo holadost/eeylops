48095a8476ead00024edb20ca29909ff6acd9fe2
Nikhil <nikhil-8987@github.com>
2021-08-15 20:17:40 -0700

    We can get rid of the ops. We won't need them!

--- go/eeylops/server/hedwig/instance_manager.go
+++ go/eeylops/server/hedwig/instance_manager.go
@@ -3,9 +3,11 @@ package hedwig
 import (
 	"context"
 	"eeylops/comm"
-	"eeylops/server/hedwig/ops"
 	"eeylops/server/replication"
 	"eeylops/server/storage"
+	"github.com/golang/glog"
+	"github.com/hashicorp/raft"
+	"time"
 )
 
 type PeerAddress struct {
@@ -36,9 +38,48 @@ func NewClusterController(opts *InstanceManagerOpts) *InstanceManager {
 	return &im
 }
 
-func (im *InstanceManager) Produce(ctx context.Context, in *comm.PublishRequest) error {
-	op := ops.NewPublishOpForSingleNode(ctx, im.replicationController, im.storageController, in, im.fsm)
-	return op.Execute()
+func (im *InstanceManager) Produce(ctx context.Context, req *comm.PublishRequest) error {
+	if len(req.GetTopicName()) == 0 {
+		glog.Errorf("Invalid topic name. Req: %v", req.GetTopicName(), req)
+		return ErrInvalidArg
+	}
+	if req.GetPartitionId() < 0 {
+		glog.Errorf("Invalid partition ID: %d. Req: %v", req.GetPartitionId(), req)
+		return ErrInvalidArg
+	}
+	// TODO: We will have to go through the replication controller first but for now,
+	// TODO: skip that and just go add via storage controller. However, this will also change
+	// TODO: in the future where we will simply apply to replication controller which will
+	// TODO: internally handle adding it to the storage controller. We just have to wait for
+	// TODO: that result.
+	appendCmd := AppendMessage{
+		TopicName:   req.GetTopicName(),
+		PartitionID: int(req.GetPartitionId()),
+		Data:        req.GetValues(),
+		Timestamp:   time.Now().UnixNano(),
+	}
+	cmd := Command{
+		CommandType:   KAppendCommand,
+		AppendCommand: appendCmd,
+	}
+	data := Serialize(&cmd)
+	log := raft.Log{
+		Index:      uint64(time.Now().UnixNano()),
+		Term:       0,
+		Type:       0,
+		Data:       data,
+		Extensions: nil,
+		AppendedAt: time.Time{},
+	}
+	appErr := im.fsm.Apply(&log)
+	retErr, err := appErr.(error)
+	if !err {
+		glog.Fatalf("Invalid return type for publish command. Expected error, got something else")
+	}
+	if retErr != nil {
+		glog.Fatalf("Unable to apply to FSM due to err: %s", retErr.Error())
+	}
+	return nil
 }
 
 func (im *InstanceManager) Consume() {
