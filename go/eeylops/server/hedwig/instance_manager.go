b50a990c95d5112845ff7f4ea767343ae462346b
Nikhil <nikhil-8987@github.com>
2021-08-15 20:28:34 -0700

    So far so good. Instance manager for single node is beginning to take shape. Hopefully, we should be done after that!

--- go/eeylops/server/hedwig/instance_manager.go
+++ go/eeylops/server/hedwig/instance_manager.go
@@ -3,6 +3,7 @@ package hedwig
 import (
 	"context"
 	"eeylops/comm"
+	"eeylops/server/base"
 	"eeylops/server/replication"
 	"eeylops/server/storage"
 	"github.com/golang/glog"
@@ -94,20 +95,122 @@ func (im *InstanceManager) Subscribe() {
 
 }
 
-func (im *InstanceManager) Commit() {
+func (im *InstanceManager) Commit(ctx context.Context, req *comm.CommitRequest) error {
+	if len(req.GetTopicName()) == 0 {
+		glog.Errorf("Invalid topic name. Req: %v", req.GetTopicName(), req)
+		return ErrInvalidArg
+	}
+	if req.GetPartitionId() < 0 {
+		glog.Errorf("Invalid partition ID: %d. Req: %v", req.GetPartitionId(), req)
+		return ErrInvalidArg
+	}
+	commitCmd := CommitMessage{
+		TopicName:   req.GetTopicName(),
+		PartitionID: int(req.GetPartitionId()),
+		Offset:      base.Offset(req.GetOffset()),
+		ConsumerID:  req.GetSubscriberId(),
+	}
+	cmd := Command{
+		CommandType:   KCommitCommand,
+		CommitCommand: commitCmd,
+	}
+	data := Serialize(&cmd)
+	log := raft.Log{
+		Index:      uint64(time.Now().UnixNano()),
+		Term:       0,
+		Type:       0,
+		Data:       data,
+		Extensions: nil,
+		AppendedAt: time.Time{},
+	}
+	appErr := im.fsm.Apply(&log)
+	retErr, err := appErr.(error)
+	if !err {
+		glog.Fatalf("Invalid return type for commit command. Expected error, got something else")
+	}
+	if retErr != nil {
+		glog.Fatalf("Unable to apply to FSM due to err: %s", retErr.Error())
+	}
+	return nil
 
 }
 
-func (im *InstanceManager) GetLastCommitted() {
+func (im *InstanceManager) GetLastCommitted(ctx context.Context) {
 
 }
 
-func (im *InstanceManager) AddTopic() {
-
+func (im *InstanceManager) AddTopic(ctx context.Context, req *comm.CreateTopicRequest) error {
+	topic := req.GetTopic()
+	if len(topic.GetTopicName()) == 0 {
+		glog.Errorf("Invalid topic name. Req: %v", topic.GetTopicName(), req)
+		return ErrInvalidArg
+	}
+	if len(topic.GetPartitionIds()) == 0 {
+		glog.Errorf("No partitions provided while creating topic")
+		return ErrInvalidArg
+	}
+	var prtIds []int
+	for _, id := range topic.GetPartitionIds() {
+		prtIds = append(prtIds, int(id))
+	}
+	addTopicMsg := AddTopicMessage{Topic: base.Topic{
+		Name:         topic.GetTopicName(),
+		PartitionIDs: prtIds,
+		TTLSeconds:   int(topic.TtlSeconds),
+		ToRemove:     false,
+	}}
+	cmd := Command{
+		CommandType:     KAddTopicCommand,
+		AddTopicCommand: addTopicMsg,
+	}
+	data := Serialize(&cmd)
+	log := raft.Log{
+		Index:      uint64(time.Now().UnixNano()),
+		Term:       0,
+		Type:       0,
+		Data:       data,
+		Extensions: nil,
+		AppendedAt: time.Time{},
+	}
+	appErr := im.fsm.Apply(&log)
+	retErr, err := appErr.(error)
+	if !err {
+		glog.Fatalf("Invalid return type for commit command. Expected error, got something else")
+	}
+	if retErr != nil {
+		glog.Fatalf("Unable to apply to FSM due to err: %s", retErr.Error())
+	}
+	return nil
 }
 
-func (im *InstanceManager) RemoveTopic() {
-
+func (im *InstanceManager) RemoveTopic(ctx context.Context, req *comm.RemoveTopicRequest) error {
+	if len(req.GetTopicName()) == 0 {
+		glog.Errorf("Invalid topic name. Req: %v", req.GetTopicName(), req)
+		return ErrInvalidArg
+	}
+	rmTopicMsg := RemoveTopicMessage{TopicName: req.GetTopicName()}
+	cmd := Command{
+		CommandType:        KRemoveTopicCommand,
+		RemoveTopicCommand: rmTopicMsg,
+	}
+	data := Serialize(&cmd)
+	log := raft.Log{
+		Index:      uint64(time.Now().UnixNano()),
+		Term:       0,
+		Type:       0,
+		Data:       data,
+		Extensions: nil,
+		AppendedAt: time.Time{},
+	}
+	appErr := im.fsm.Apply(&log)
+	retErr, err := appErr.(error)
+	if !err {
+		glog.Fatalf("Invalid return type for commit command. Expected error, got something else")
+	}
+	if retErr != nil {
+		glog.Fatalf("Unable to apply to FSM due to err: %s", retErr.Error())
+	}
+	return nil
 }
 
 func (im *InstanceManager) GetTopic() {
