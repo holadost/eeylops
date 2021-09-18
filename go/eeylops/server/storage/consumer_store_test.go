f1c32c9eaef5f94b37c0aa277b64461939a56f7f
Nikhil <nikhil-8987@github.com>
2021-09-18 14:56:51 -0700

    Consumer store now provides a method to remove entries for non existent topics. This method can be invoked by the broker store manager periodically to remove these entries.

--- go/eeylops/server/storage/consumer_store_test.go
+++ go/eeylops/server/storage/consumer_store_test.go
@@ -48,3 +48,73 @@ func TestConsumerStore(t *testing.T) {
 	}
 	glog.Infof("Consumer store test finished successfully")
 }
+
+func TestConsumerStoreCleanup(t *testing.T) {
+	testutil.LogTestMarker("TestConsumerStoreCleanup")
+	testDir := testutil.CreateFreshTestDir("TestConsumerStoreCleanup")
+	cs := NewConsumerStore(testDir, "1")
+	successTopID := base.TopicIDType(1)
+	failTopID := base.TopicIDType(2)
+	partitionID := uint(100)
+	consumers := []string{"consumer1", "consumer2", "consumer3", "consumer4"}
+	topIds := []base.TopicIDType{successTopID, failTopID}
+	glog.Infof("Registering consumers!")
+	for _, consumerID := range consumers {
+		for _, topid := range topIds {
+			if err := cs.RegisterConsumer(consumerID, topid, partitionID, 100); err != nil {
+				glog.Fatalf("Unable to register consumer due to err: %s", err.Error())
+				return
+			}
+		}
+
+	}
+
+	glog.Infof("Committing offsets for consumers!")
+	numCommits := 50
+	for ii := 0; ii < numCommits; ii++ {
+		if ii%5 == 0 {
+			if err := cs.Close(); err != nil {
+				glog.Fatalf("Failed to close the store due to err: %s", err.Error())
+				return
+			}
+			cs = NewConsumerStore(testDir, "1")
+		}
+		for _, consumerID := range consumers {
+			for _, topid := range topIds {
+				if err := cs.Commit(consumerID, topid, partitionID, base.Offset(ii), 101); err != nil {
+					glog.Fatalf("Unable to commit for consumer due to err: %s", err.Error())
+					return
+				}
+				num, err := cs.GetLastCommitted(consumerID, topid, partitionID)
+				if err != nil {
+					glog.Fatalf("Unable to get last committed offset due to err: %s", err.Error())
+					return
+				}
+				if num != base.Offset(ii) {
+					glog.Fatalf("Value Mismatch. Expected: %d, Got: %d", uint64(ii), num)
+					return
+				}
+			}
+		}
+	}
+	if err := cs.Close(); err != nil {
+		glog.Fatalf("Failed to close the store due to err: %s", err.Error())
+		return
+	}
+	cs = NewConsumerStore(testDir, "1")
+
+	glog.Infof("Removing non existent topic consumers from the store")
+	cs.RemoveNonExistentTopicConsumers([]base.TopicIDType{failTopID})
+	for _, consumerID := range consumers {
+		_, err := cs.GetLastCommitted(consumerID, failTopID, partitionID)
+		if err == nil {
+			glog.Fatalf("Got a committed offset even when we were not expecting it")
+			return
+		}
+		_, err = cs.GetLastCommitted(consumerID, successTopID, partitionID)
+		if err != nil {
+			glog.Fatalf("Error while expecting last committed offset: %v", err)
+			return
+		}
+	}
+}
